8ae3b2374b72d1e6c225c5cc482242426dd50d5d
Vikramjit <vikramjitsingh551@gmail.com>
2022-01-24 22:22:48 -0700

    Cloudflare tryout

--- components/global/main/InfoCard.js
+++ components/global/main/InfoCard.js
@@ -2,6 +2,12 @@ import Image from "next/image";
 import Button from "../Button";
 
 export default function InfoCard(props) {
+    const cloudflareImageLoader = ({ src, width, quality }) => {
+        if (!quality) {
+          quality = 75
+        }
+        return `https://image-handler.vc551.workers.dev?width=${width}&quality=${quality}&image=https://vc551.com${src}`
+      }
     var image = <></>
     if (props.data?.image) {
         image = <Image
--- next.config.js
+++
@@ -1,6 +0,0 @@
-const cloudflareImageLoader = ({ src, width, quality }) => {
-    if (!quality) {
-      quality = 75
-    }
-    return `https://image-handler.vc551.workers.dev?width=${width}&quality=${quality}&image=https://vc551.com${src}`
-  }
