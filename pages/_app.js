485b09f3a3d881813e5e959cd0e2e073cad9e60a
Vikramjit <vikramjitsingh551@gmail.com>
2022-02-06 01:40:00 -0700

    Added OG:Image Metadata

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }) {
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"></link>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+                <meta property="og:image" content="https://vc551.com/images/og_image.png" />
             </Head>
             <Script
                 src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
