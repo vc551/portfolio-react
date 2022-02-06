88f988eb74fc83851eaf2b4fb9a9b71d9dc54e95
Vikramjit <vikramjitsingh551@gmail.com>
2022-02-05 20:26:02 -0700

    Added Academic Qualifications

--- src/main/qualifications/cards.js
+++ src/main/qualifications/cards.js
@@ -22,10 +22,44 @@ export const qualificationsData = [
                 height:32,
                 header:"August 2019 - May 2020",
                 image:"/images/fultonschoolsofengineering.svg",
-                imagealt:"ASU Ira A. Fulton Schools of Engineering",
+                imagealt: "Ira A. Fulton Schools of Engineering",
                 title: "Undergraduate Teaching Assistant",
                 subtitle: "Ira A. Fulton Schools of Engineering",
                 text: "Served as a student mentor and an academic assistant in a Digital Design course. Conducted various activities like exam reviews, course surveys, lead lectures, etc. to ensure student success.",
+            }
+        ]
+    },
+    {
+        backstyle: "light",
+        textstyle: "dark",
+        heading: "Academic Qualifications",
+        paragraph: "Below are the most recent of my academic qualifications.",
+        cards: [
+            {
+                height:33.5,
+                header:"August 2018 - May 2022",
+                image:"/images/fultonschoolsofengineering.svg",
+                imagealt: "Ira A. Fulton Schools of Engineering",
+                title: "Computer Science, BS",
+                subtitle: "Ira A. Fulton Schools of Engineering, Arizona State University-Tempe, Arizona (US)",
+                text: "Bachelor's degree majoring in Computer Science from Arizona State University's Tempe campus. Graduating May 2022.",
+                buttontext: "Ira A. Fulton Schools of Engineering",
+                buttonlink: "https://engineering.asu.edu",
+                buttonglow: false,
+                buttonclass: "btn btn-primary container-fluid"
+            },
+            {
+                height:33.5,
+                header:"April 2014 - April 2018",
+                image:"/images/dav.svg",
+                imagealt:"DAV College Managing Committee",
+                title: "High Schooling",
+                subtitle: "DAV Public School, Bhai Randhir Singh Nagar, Ludhiana, Punjab (IN)",
+                text: "Completed high schooling with a CGPA-10 (out of 10). Studied Non-Medical (Physics, Chemistry, Math), and Computer Science.",
+                buttontext: "DAV Public School, BRS Nagar, Ludhiana",
+                buttonlink: "http://davbrsnagar.org",
+                buttonglow: false,
+                buttonclass: "btn btn-primary container-fluid"
             }
         ]
     }
