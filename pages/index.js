32dad24a8003fbee229fbedeb0c0230c2cfc7cd1
Vikramjit <vikramjitsingh551@gmail.com>
2022-03-16 19:21:34 -0700

    Added Custom Favicon

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,10 @@ export default function Home() {
 		<div className='full-height'>
 			<Head>
 				<title>Vikramjit Singh</title>
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
 				<meta name="theme-color" content="#212529" />
 			</Head>
 			<Nav
